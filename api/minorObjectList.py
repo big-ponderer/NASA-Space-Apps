7af4d7a73a91358ddc16cc79d57cbb2ca87dac6f
SnackOverflow707 <131423796+SnackOverflow707@users.noreply.github.com>
2024-10-05 12:34:25 -0700

    Add files via upload

---
+++ api/minorObjectList.py
@@ -0,0 +1,48 @@
+import pandas as pd
+class Asteroid:
+    def __init__(self, name, diameter, mass, period, displayName, NEO, PHA ):
+        self.name = name
+        self.diameter = diameter
+        self.mass = mass
+        self.period = period
+        self.displayName = displayName
+        #The two are boolean
+        self.NEO = NEO
+        self.PHA = PHA
+        #Set xyz positions to be defined later
+        self.sunCoords = [0,0,0]
+        self.sectorCoords = [0,0,0]
+        #Set quadrant (row and column)
+        self.quadrant = [0,0]
+     
+    def printData(self):
+        print(f"Asteroid(name={self.name}, diameter={self.diameter}, mass={self.mass}, period={self.period}, displayName={self.displayName}, NEO={self.NEO}, PHA={self.PHA})")
+
+    def returnQuad(self):
+        return self.quadrant
+    
+    
+#Change this to change asteroid file
+asteroidListCSV = pd.read_csv("api/routers/MinorObjects_code_and_file/sbdb_query_results.csv")
+
+
+
+print(asteroidListCSV.head())
+
+#main data structure
+asteroidList = []
+
+for index, row in asteroidListCSV.iterrows():
+    
+    asteroid = Asteroid(
+        name=row['full_name'],
+        diameter=row['diameter'],
+        mass=row['GM'],
+        period=row['per_y'],
+        displayName=row['name'],
+        NEO = row['neo'] == 'Y',
+        PHA = row['pha'] == 'Y'
+    )
+    asteroidList.append(asteroid)
+
+asteroidList[0].printData()
