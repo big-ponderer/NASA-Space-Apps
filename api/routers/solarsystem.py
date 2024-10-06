17a40281257191778bd0dcb548a66ac589ff9948
Leo <leo.lj.zhu@gmail.com>
2024-10-06 03:19:59 -0700

    changed to one endpoint

--- api/routers/solarsystem.py
+++ api/routers/solarsystem.py
@@ -18,8 +18,20 @@ async def read_item():
         for i in range(len(mainArray)):
             one_layer = []
             for j in range(len(mainArray[i])):
-                density = len(mainArray[i][j].asteroidSectorList)
-                one_layer.append({"id": [i, j], "density": density})
+                output_dic = {}
+                output_dic["id"] = [i, j]
+                output_dic["density"] = len(mainArray[i][j].asteroidSectorList)
+                asteroid_list = []
+                asteroid_dict = {}
+                for asteroid in mainArray[i][j].asteroidSectorList:
+                    asteroid_dict = {
+                        "position": asteroid.sunCoords, 
+                        "velocity": asteroid.velocity,
+                        "radius": asteroid.diameter
+                    }
+                asteroid_list.append(asteroid_dict)
+                output_dic["asteroids"] = asteroid_list
+                one_layer.append(output_dic)
             sectors.append(one_layer)
         #print(sectors, "working")
         print(type(sectors))
