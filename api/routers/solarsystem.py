7520ad6a102e4a12f1d6998b39e815ea1f931fa7
Leo <leo.lj.zhu@gmail.com>
2024-10-06 02:03:10 -0700

    end point ready

--- api/routers/solarsystem.py
+++ api/routers/solarsystem.py
@@ -13,13 +13,20 @@ async def read_item():
     try:
         asteroids = AllAsteroids("sbdb_query_results-3.csv")
         mainArray =  await asteroids.updateAllCoords()
-        sectors = {}
-        """for i in ran
+        #print(mainArray)
+        sectors = []
+        for i in range(len(mainArray)):
+            for j in range(len(mainArray[i])):
+                density = len(mainArray[i][j].asteroidSectorList)
+                sectors.append({"id": [i, j], "density": density})
+        #print(sectors, "working")
+        print(type(sectors))
         ret = {
             "radii": asteroids.testSector.circleList,
             "angles": asteroids.testSector.waveTheta*2*np.pi/360,
-            "sectors": mainArray
-        }"""
-        return mainArray
+            "sectors": sectors
+        }
+        #print(ret)
+        return ret
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
